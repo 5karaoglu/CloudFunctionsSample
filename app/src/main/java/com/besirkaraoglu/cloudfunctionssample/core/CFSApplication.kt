a5f11c42b6276fbd1973861608f8ff5f5f5e4b00
Beşir Karaoğlu <50145106+5karaoglu@users.noreply.github.com>
2022-12-14 15:15:45 +0300

    add hilt

--- app/src/main/java/com/besirkaraoglu/cloudfunctionssample/core/CFSApplication.kt
+++ app/src/main/java/com/besirkaraoglu/cloudfunctionssample/core/CFSApplication.kt
@@ -1,6 +1,8 @@
 package com.besirkaraoglu.cloudfunctionssample.core
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class CFSApplication: Application() {
 }
